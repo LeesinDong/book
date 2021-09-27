bbf988e5f230052154bd409c3d7b5ed771907eae
LeesinDong <32248827@qq.com>
2021-09-27 15:29:57 +0800

    fixed

--- EffectiveJava/src/main/java/no22/Test.java
+++ EffectiveJava/src/main/java/no22/Test.java
@@ -1,5 +1,7 @@
 package no22;
 
+import java.util.List;
+
 /**
  * @author Leesin Dong
  * @since Created in 2021/9/26 10:40 下午
@@ -10,7 +12,9 @@ public class Test {
     public static final String CONST_TEST = "const test";
     public static final String CONST_TEST1 = "const test1";
     public static final String CONST_TEST2 = "const test1";
-    
+
+    // will warn
+    // List<String>[] s = new List<String>[1];
 }
 
 
