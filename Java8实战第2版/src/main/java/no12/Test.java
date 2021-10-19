4ee757bc9b70b8dd45c50fbedff2a6f9fddb092a
LeesinDong <32248827@qq.com>
2021-10-20 07:40:55 +0800

    fixed

--- Java8实战第2版/src/main/java/no12/Test.java
+++ Java8实战第2版/src/main/java/no12/Test.java
@@ -1,9 +1,14 @@
 package no12;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
+import java.time.*;
+import java.time.chrono.JapaneseDate;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
+import java.time.zone.ZoneRules;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 /**
  * @author Leesin Dong
@@ -24,5 +29,36 @@ public class Test {
 
         LocalDate plus = LocalDate.now().plus(1, ChronoUnit.DAYS);
         System.out.println(plus);
+
+
+        String format = LocalDate.of(2014, 3, 2)
+                .format(DateTimeFormatter.ofPattern("d. MMMM yyyy", Locale.ITALIAN));
+        LocalDate parse = LocalDate.parse(format, DateTimeFormatter.ofPattern("d. MMMM yyyy", Locale.ITALIAN));
+        System.out.println(format);
+        System.out.println(parse);
+
+        ZoneId of2 = ZoneId.of("Europe/Rome");
+        ZoneId zoneId = TimeZone.getDefault().toZoneId();
+        System.out.println(of2);
+        System.out.println(zoneId);
+
+        LocalDate of3 = LocalDate.of(2014, 2, 4);
+        ZonedDateTime zonedDateTime = of3.atStartOfDay(zoneId);
+        System.out.println(zonedDateTime);
+        ZonedDateTime zonedDateTime1 = Instant.now().atZone(zoneId);
+
+        // LocalDateTime of4 = LocalDateTime.of(1213, 3, 3, 12, 3, 4);
+        // Instant instant = of4.toInstant((ZoneOffset) zoneId);
+
+        Date date = new Date();
+
+        JapaneseDate from = JapaneseDate.from(of3);
+
+        ZoneRules rules = of2.getRules();
+        System.out.println(rules);
+
+        LocalDateTime.now();
+
+
     }
 }
